8938bafe4a3feb90281f2b1fd47617ac1fbcafe2
rodrigo-maestro <rodrigo.maestro98@gmail.com>
2023-07-07 14:21:12 -0300

    evento click buscando arquivo

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function selectFile(){
-    console.log("teste")
+const fileInput = document.getElementById('my_file');
+
+function getFile(){
+    fileInput.click();
 }
+
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    
+    if (!file || file == null) {
+        return;
+    }
+
+    if (file.size/1000/1000 > 2) {
+        alert("only files smaller than 2MB")
+    }
+    
+    console.log(file);
+});
